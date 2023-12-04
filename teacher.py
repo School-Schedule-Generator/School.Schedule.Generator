7d4422d2aa3c9c20068021a1d834f5b1de4e4809
Olaf 3 <91374689+OFALOFAL@users.noreply.github.com>
2023-12-04 22:26:36 +0100

    Started changing creation of schedule to fit teachers

---
+++ teacher.py
@@ -0,0 +1,8 @@
+class Teacher:
+    def __init__(self, name, surname, possible_subjects, start_hour_index, end_hour_index, days):
+        self.name = name
+        self.surname = surname
+        self.possible_subjects = possible_subjects
+        self.start_hour_index = start_hour_index
+        self.end_hour_index = end_hour_index
+        self.days = days
