3d3fbbe061fe97257ccd2687a861e657c928b1ed
Olaf 3 <91374689+OFALOFAL@users.noreply.github.com>
2024-02-13 17:46:03 +0100

    Update settings.py

--- generatorApp/schoolSchedule/settings.py
+++ generatorApp/schoolSchedule/settings.py
@@ -10,6 +10,7 @@ class Settings:
         # if debug create default names
         self.TEST_DATA_PATH = '../../testdata'
         self.BASE_DATA_PATH = '../static/data'
+        self.DATABASE_NAME = 'db'
         self.DF_NAMES = ['SSG_LESSON_HOURS', 'SSG_SUBJECT_NAMES', 'SSG_SUBJECTS', 'SSG_TEACHERS', 'SSG_CLASSES',
                          'SSG_CLASSROOMS', 'SSG_CLASSROOM_TYPES']
 
@@ -25,6 +26,37 @@ class Settings:
                             'supervising_teacher', 'starting_lesson_hour_id'],
             'SSG_CLASSROOMS': ['classroom_id', 'classroom_name', 'type_id'],
             'SSG_CLASSROOM_TYPES': ['type_id', 'description']
+        }
+
+        self.SQLTABLES = {
+            'SSG_LESSON_HOURS': {
+                'Name': 'LessonHours',
+                'Columns': self.COLUMN_NAMES['SSG_LESSON_HOURS']
+            },
+            'SSG_SUBJECT_NAMES': {
+                'Name': 'SubjectNames',
+                'Columns': self.COLUMN_NAMES['SSG_SUBJECT_NAMES']
+            },
+            'SSG_SUBJECTS': {
+                'Name': 'Subject',
+                'Columns': self.COLUMN_NAMES['SSG_SUBJECTS']
+            },
+            'SSG_TEACHERS': {
+                'Name': 'Teachers',
+                'Columns': self.COLUMN_NAMES['SSG_TEACHERS']
+            },
+            'SSG_CLASSES': {
+                'Name': 'Classes',
+                'Columns': self.COLUMN_NAMES['SSG_CLASSES']
+            },
+            'SSG_CLASSROOMS': {
+                'Name': 'Classrooms',
+                'Columns': self.COLUMN_NAMES['SSG_CLASSROOMS']
+            },
+            'SSG_CLASSROOM_TYPES': {
+                'Name': 'ClassroomTypes',
+                'Columns': self.COLUMN_NAMES['SSG_CLASSROOM_TYPES']
+            }
         }
 
 
